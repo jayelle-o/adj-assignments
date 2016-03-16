33e2724ecc4b02dc3b0cf2e12d81b95c23c7da4e
Chase Davis <chase.davis@gmail.com>
2016-03-15 23:43:09 -0400

    Jailscrape

--- assignments/data-cleaning/cleaner.py
+++ assignments/data-cleaning/cleaner.py
@@ -1,1 +1,20 @@
-# Your code goes here
+# -*- coding: utf-8 -*-
+import csv
+import string
+
+csvfile = open('./data/cleanme.csv', 'r')
+outfile = open('./data/cleanme-clean.csv', 'w')
+specials = "&NBSP;"
+
+reader = csv.DictReader(csvfile)
+
+writer = csv.DictWriter(outfile, reader.fieldnames)
+
+writer.writeheader()
+    
+for row in reader:
+    for k, v in row.iteritems():
+        row[k]=row[k].upper()
+        row["zip"]=row["zip"].zfill(5)
+        row[k] = row[k].replace(specials, ' ')
+    print row
