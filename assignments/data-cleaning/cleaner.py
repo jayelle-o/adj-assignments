432a3b74729c0784865600ce198bdd0c17c7e902
Jennifer Lu <julenn@gmail.com>
2016-03-16 15:34:01 -0500

    changed files to reflect my hw assignment, cleaner.py

--- assignments/data-cleaning/cleaner.py
+++ assignments/data-cleaning/cleaner.py
@@ -1,20 +1,31 @@
-# -*- coding: utf-8 -*-
 import csv
-import string
 
 csvfile = open('./data/cleanme.csv', 'r')
-outfile = open('./data/cleanme-clean.csv', 'w')
-specials = "&NBSP;"
+outfile = open('./data/allclean.csv', 'w')
+
+#print csvfile.read()
 
 reader = csv.DictReader(csvfile)
-
 writer = csv.DictWriter(outfile, reader.fieldnames)
 
 writer.writeheader()
-    
+
 for row in reader:
-    for k, v in row.iteritems():
-        row[k]=row[k].upper()
-        row["zip"]=row["zip"].zfill(5)
-        row[k] = row[k].replace(specials, ' ')
-    print row
+
+    # uppercase all fields:
+    # first_name,mid_init,last_name,addr,city,state,zip,check_no,check_date,amount
+    row['first_name'] = row['first_name'].upper()
+    row['mid_init'] = row['mid_init'].upper()
+    row['last_name'] = row['last_name'].upper()
+    row['addr'] = row['addr'].upper()
+    row['city'] = row['city'].upper()
+    row['state'] = row['state'].upper()
+
+    row['zip'] = row['zip'].zfill(5)  # add zeroes to any ZIP codes
+    row['city'] = row['city'].replace("&NBSP;", " ") # delete non-breaking spaces
+
+
+    if int(row['amount']) >= 1000:
+
+        print row
+        writer.writerow(row)
